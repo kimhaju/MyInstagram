4669779780c79ef954bf365adbad1ff695b4c6ac
haju <haju0321@gmail.com>
2021-09-26 19:39:21 +0900

    Initial Commit

---
+++ MyInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyInstagram
+//
+//  Created by haju Kim on 2021/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
