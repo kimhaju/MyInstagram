2067d61072e54115c0e8e3976e08186723099c57
haju <haju0321@gmail.com>
2021-09-30 12:37:07 +0900

    데이터 베이스 연결 여부 확인 (파이어 스토어짱!)

--- MyInstagram/API/AuthService.swift
+++ MyInstagram/API/AuthService.swift
@@ -16,8 +16,28 @@ struct AuthCredentials {
     let profileImage: UIImage
 }
 
+//->파이어 스토어에 저장할 코드 생성 
 struct AuthService {
-    static func registerUser(withCredential credentials: AuthCredentials){
-        print("회원가입 유저 연결 확인: \(credentials)")
+    static func registerUser(withCredential credentials: AuthCredentials, completion: @escaping(Error?) -> Void){
+        
+        imageUploader.uploadImage(image: credentials.profileImage) { imageURL in
+            Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { result, error in
+                
+                if let error = error {
+                    print("회원가입에 실패했습니다. \(error.localizedDescription)")
+                    return
+                }
+                guard let uid = result?.user.uid else {return}
+                
+                let data: [String: Any] = ["email": credentials.email,
+                                           "fullname": credentials.fullname,
+                                           "profileImageURL" : imageURL,
+                                           "uid": uid,
+                                           "username" : credentials.username
+                ]
+                Firestore.firestore().collection("users").document(uid).setData(data, completion: completion)
+            }
+        }
+       
     }
 }
--- MyInstagram/Controller/Authentication/RegistrationController.swift
+++ MyInstagram/Controller/Authentication/RegistrationController.swift
@@ -55,7 +55,7 @@ class RegistrationController: UIViewController {
     private let alreadyAccountButton: UIButton = {
         let button = UIButton(type: .system)
         
-        button.attributedTitle(firstPart: "이미 계정이 있으신가요?", secondPart: "Sign Up")
+        button.attributedTitle(firstPart: "이미 계정이 있으신가요?", secondPart: "Log In")
         button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
         return button
     }()
@@ -76,12 +76,18 @@ class RegistrationController: UIViewController {
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
         guard let fullname = fullnameTextField.text else { return }
-        guard let username = usernameTextField.text else { return }
+        guard let username = usernameTextField.text?.lowercased() else { return }
         guard let profileImage = self.profileImage else { return }
         
         let credentials = AuthCredentials(email: email, password: password, fullname: fullname, username: username, profileImage: profileImage)
         
-        AuthService.registerUser(withCredential: credentials)
+        AuthService.registerUser(withCredential: credentials) { error in
+            if let error = error {
+                print("연결하는데 문제가 생겼습니다! \(error.localizedDescription)")
+                return
+            }
+            print("파이어 스토어에 연결하는데 성공했습니다!")
+        }
     }
     
     @objc func handleShowLogin() {
